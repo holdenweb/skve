2b70bc061a0f6b96b94392081a27d595d8fd9861
Steve Holden <steve@holdenweb.com>
2023-08-24 15:40:06 +0100

    Successful initial experiment with push-screen, though I'm not convinced that I fully understand screen handling yet (and I'm not sure I want to start poring through the source).
    Sorry about the hunk o'code. Been hacking and learning.

--- src/textutils/people.py
+++ src/textutils/people.py
@@ -1,7 +1,11 @@
 from textual.app import App, ComposeResult
 from textual.validation import Function, Number, ValidationResult, Validator
-from textual.widgets import Input, Label, Pretty, Static, Footer, Button, DataTable
-from textual.containers import Container, Horizontal, HorizontalScroll, Vertical, VerticalScroll, Center
+from textual.widgets import Input, Label, Pretty, Static, Footer, Button, DataTable, TextArea
+from textual.containers import Container, Horizontal, HorizontalScroll, Vertical, VerticalScroll, Center, Grid
+from textual.screen import Screen, ModalScreen
+from textual.events import InputEvent
+from textual.document import Document
+
 import mongoengine
 
 import importlib.resources as ir
@@ -12,24 +16,68 @@ from itertools import cycle
 from .models import Person
 
 os.environ["TEXTUAL"] = "debug,devtools"
+EMPTY_DOC = Document("")
 
+class NewRecordScreen(ModalScreen):
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            Horizontal(
+                Input(id="input-key"),
+                TextArea(document=EMPTY_DOC, id="input-value"),
+                id="input-row"
+            ),
+            Center(
+                Label("Are you sure you want to quit?", id="question"),
+                Horizontal(
+                    Button("Save", variant="error", id="save"),
+                    Button("Cancel", variant="primary", id="cancel")
+                ),
+                id="label-and-buttons"
+            ),
+            id="dialog",
+        )
+
+    def on_button_pressed(self: Screen, event: Button.Pressed) -> None:
+        for node in self.walk_children():
+            print(node, node.id)
+        id = event.button.id
+        if id == "save":
+            retval = (self.query_one("#input-key").value,
+                      self.query_one("#input-value").text)
+            self.dismiss(result=retval)
+        elif id == "cancel":
+            retval = None
+            self.dismiss()
+        else:
+            raise ValueError("Press of unknown button {id!r}")
+
+class InputStripe(Horizontal):
+    def compose(self):
+        yield Label("Search: ")
+        yield Input(
+                placeholder="Enter (part of) a person's name...",
+                id="in-val",
+        )
+        yield Button(label="+", variant="primary", id="add-person")
+
+    def on_button_pressed(self):
+        app.push_screen(NewRecordScreen(), self.stash_result)
+
+    def stash_result(self, return_value):
+        print("DIALOG RETURNED", return_value)
 
 class PeopleApp(App):
 
     CSS_PATH = "people.css"
 
-    def __init__(self):
-        super().__init__()
+    def on_mount(self):
+        self.push_screen(MainScreen())
+        self.screen.styles.background = "blue"
+
+class MainScreen(Screen):
 
     def compose(self) -> ComposeResult:
-        yield Horizontal(
-            Label("Name: "),
-            Input(
-                placeholder="Enter (part of) a person's name...",
-                id="in_val,"
-                ),
-            id="question-box"
-        )
+        yield InputStripe(id="question-box")
         yield Horizontal(
             VerticalScroll(
                 id="buttons"
@@ -40,13 +88,12 @@ class PeopleApp(App):
             ),
             id="body"
         )
-
         yield Center(Label(f"Initial message"), id="message")
-
 
     def on_input_changed(self, event: Input.Changed) -> None:
         # Updating the UI to show the reasons why validation failed
-        for btn in self.query(Button):
+        print("BOO", event)
+        for btn in self.query(".person-button"):
             btn.remove()
         in_string = self.query_one(Input)
         value = in_string.value
@@ -55,24 +102,24 @@ class PeopleApp(App):
         if count == 0:
             self.replace_message("No matches")
         else:
-            self.bg_class = cycle(("bg-yellow", "bg-red"))
+            app.bg_class = cycle(("bg-yellow", "bg-red"))
             self.replace_message(f"{self.me_query.count()} people")
             self.btns = []
             if value:
                 for acr in self.me_query:
                     self.query_one("#buttons").mount(
-                        btn := PersonButton(acr)
+                        btn := PersonButton(acr, classes="person-button")
                     )
                     self.btns.append(btn)
 
     def replace_message(self, msg):
         self.query_one("#message").remove()
-        self.mount(Center(Label(msg), id="message"))
+        self.mount(Center(Label(msg, id="message")))
 
 class PersonButton(Button):
 
-    def __init__(self, person):
-        super().__init__(person.name)
+    def __init__(self, person, **kw):
+        super().__init__(person.name, **kw)
         self.person = person
 
     def on_button_pressed(self):
