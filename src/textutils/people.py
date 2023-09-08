c700827eecf41de976351f0425fac16dbe3bd595
Steve Holden <steve@holdenweb.com>
2023-09-08 12:06:52 +0100

    Rip out the mongoengine in favour of a partable alternative.

--- src/textutils/people.py
+++ src/textutils/people.py
@@ -7,13 +7,10 @@ from rich.text import Text
 
 from textutils.key_value_edit import KeyValueEditScreen
 from textutils.lib import SaveCancel
-
-import mongoengine
+from textutils.test_store import people_matching, update_person
 
 import os
 from itertools import cycle
-
-from .models import Person
 
 os.environ["TEXTUAL"] = "debug,devtools"
 
@@ -42,20 +39,20 @@ class MainScreen(Screen):
 
     def on_input_changed(self, event: Input.Changed) -> None:
         for btn in self.query(".person-button"):
-            btn.remove()
+            btn.remove()  # TODO: Use remove_children on parent
         in_string = self.query_one(Input)
         value = in_string.value
-        self.me_query = Person.objects(name__icontains=value)
-        count = self.me_query.count()
+        self.me_query = people_matching(value)
+        count = len(self.me_query)
         if count == 0:
             self.replace_message("No matches")
         else:
-            self.replace_message(f"{self.me_query.count()} people")
+            self.replace_message(f"{count} people")
             self.btns = []
             if value:
-                for acr in self.me_query:
+                for person in self.me_query:
                     self.query_one("#buttons").mount(
-                        btn := PersonButton(acr, classes="person-button")
+                        btn := PersonButton(person, classes="person-button")
                     )
                     self.btns.append(btn)
 
@@ -75,20 +72,17 @@ class InputStripe(Widget):
 class PersonButton(Button):
 
     def __init__(self, person, **kw):
-        super().__init__(person.name, **kw)
+        super().__init__(person['name'], **kw)
         self.person = person
 
     def on_button_pressed(self):
         app.bg_class = cycle(("bg-one", "bg-two"))
-        app.query_one("#content").remove()
-        app.query_one("#body").mount(
-            VerticalScroll(id="content")
-        )
+        app.query_one("#content").remove_children()
         self.add_new_row("Key", "Value", clickable=False)
-        self.add_new_row("name", self.person.name, clickable=True)
-        for k in self.person._fields_ordered:
+        self.add_new_row("name", self.person['name'], clickable=True)
+        for k in self.person.keys():
             if k not in {'name', 'id'}:
-                self.add_new_row(k, getattr(self.person, k))
+                self.add_new_row(k,self.person[k])
         app.query_one("#content").mount(SaveCancel(self.save_back))
 
     def add_new_row(self, key, value, w_type=Static, clickable=True):
@@ -100,7 +94,8 @@ class PersonButton(Button):
         if save_flag:
             key_vals = dict((row.key, row.value) for row in self.app.query(".result-row") if row.clickable)
             # Might get ugly with complex field types?
-            self.person.modify(**key_vals)
+            print("KEY VALS:", key_vals)
+            update_person(self.person, key_vals)
         self.app.query_one("#content").remove_children()
 
 class ResultRow(Widget):
@@ -139,4 +134,3 @@ class ResultRow(Widget):
         self.value_field.update(Text(text=self.value))
 
 app = PeopleApp()
-dbcon = mongoengine.connect("acronyms")
